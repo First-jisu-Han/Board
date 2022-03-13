9b286b6ed529100d97944c65d867b29265b3237d
Jisu Han <weblite@naver.com>
2022-03-14 00:34:29 +0900

    게시판 이용자, MySQL에 Board DB안에 User테이블 생성 + User클래스 생성

---
+++ src/main/java/project/board/user/User.java
@@ -0,0 +1,15 @@
+package project.board.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class User {
+
+    private String userID;
+    private String userPassword;
+    private String userName;
+    private String userGender;
+    private String userEmail;
+
+}
